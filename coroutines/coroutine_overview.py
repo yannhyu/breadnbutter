b3f0dd5f3831984d2ac94a64e97fe1724cef6704
Yann Yu <yann.yu@medlytix.com>
2017-02-16 14:42:31 -0500

    starting point of coroutine

---
+++ coroutines/coroutine_overview.py
@@ -0,0 +1,14 @@
+# coroutine_overview.py
+
+# a piece of code that is sitting there ready
+# for some other manager type of program to run
+async def greeting(name):
+    return 'hey, ' + name
+
+if __name__ == '__main__':
+    g = greeting('coroutine')
+    print(g)
+    import asyncio
+    loop = asyncio.get_event_loop()
+    res = loop.run_until_complete(g)
+    print(res)
