c5fc2ee455b683abfe437c15da056645538f35e7
Ian Yu <ianyu_it@yahoo.com>
2017-03-06 21:12:07 -0500

    hey

--- how2import_module_anywhere/bin/ebrealtime/ebrealtime_app.py
+++ how2import_module_anywhere/bin/ebrealtime/ebrealtime_app.py
@@ -1,6 +1,6 @@
 # ebrealtime_app.py
 import os, sys
-lib_path = os.path.abspath(os.path.join('..', '..', 'lib/ebrealtime'))
+lib_path = os.path.abspath(os.path.join('..', '..', 'libs/ebrealtime'))
 sys.path.append(lib_path)
 
 import config
