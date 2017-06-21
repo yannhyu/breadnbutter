9888ae64bec24c1d86d3cef3fb8d86eea6c9f189
Yann Yu <yann.yu@medlytix.com>
2017-06-21 11:27:37 -0400

    add other filters

--- liteweightdjango/build_rest_api/scrum/board/views.py
+++ liteweightdjango/build_rest_api/scrum/board/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import authentication, permissions, viewsets, filters
+from .forms import TaskFilter
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
@@ -37,6 +38,7 @@ class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """API endpoint for listing and creating tasks."""
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_class = TaskFilter
     search_fields = ('name', 'description', )
     ordering_fields = ('name', 'order', 'started', 'due', 'completed', )
 
--- liteweightdjango/build_rest_api/scrum/scrum/settings.py
+++ liteweightdjango/build_rest_api/scrum/scrum/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     # 3rd party apps
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
     # Internal apps
     'board',
 )
