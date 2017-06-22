cab2e105a70e0c340703c3c20f98f296dbd20e19
Yann Yu <yann.yu@medlytix.com>
2017-06-22 16:45:46 -0400

    add counter

--- Big_Ideas_Little_Python_Code/part1.1/use_counter.py
+++ Big_Ideas_Little_Python_Code/part1.1/use_counter.py
@@ -8,6 +8,7 @@ try:
 except KeyError as ker:
     print(f'KeyError on dragon: {ker}')
 
+# Counter is modelled after 'bag' in smalltalk
 d = Counter()
 print(d['dragon'])
 
@@ -18,5 +19,12 @@ print(d)
 color_counter = Counter('red green red blue red blue green purple'.split())
 print(color_counter)
 
+print(color_counter.most_common(1))
+print(color_counter.most_common(2))
 
+# be able to list all elements is useful for analytics
+print(list(color_counter.elements()))
 
+print(list(color_counter))
+print(list(color_counter.values()))
+print(list(color_counter.items()))
