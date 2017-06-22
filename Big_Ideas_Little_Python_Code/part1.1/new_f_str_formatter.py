0dd5e159d1af570c0e5bb789d3b0035db3e8f051
Yann Yu <yann.yu@medlytix.com>
2017-06-22 15:49:53 -0400

    big ideas small modern python

---
+++ Big_Ideas_Little_Python_Code/part1.1/new_f_str_formatter.py
@@ -0,0 +1,10 @@
+# new_f_str_formatter.py
+
+x = 10
+
+print('The answer is %d today' % x)
+print('The answer is {0} today'.format(x))
+print('The answer is {a} today'.format(a=x))
+
+# available for python3.6+
+print(f'The answer is {x} today')
