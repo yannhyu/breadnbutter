b167d05af3aa472aae1f7a3630f3e921967def0b
Yann Yu <yann.yu@medlytix.com>
2017-06-27 13:06:58 -0400

    extract vars then func then class

---
+++ refactor2make_it_obvious/func_evolves_into_class_05.py
@@ -0,0 +1,50 @@
+# func_evolves_into_class_05.py
+
+MONTHS = ('January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December')
+
+class OystersGood:
+    def __init__(self, month):
+        lowered = month.lower()
+        self.r = lowered.endswith('r')
+        self.ary = lowered.endswith('ary')
+        self._result = self.r or self.ary
+    def __bool__(self): # aka __nonzero__
+        return self._result
+
+
+class TomatoesGood:
+    def __init__(self, month):
+        self.index = MONTHS.index(month)
+        self._result = 8 > self.index > 4
+
+    def __bool__(self): # aka __nonzero__
+        return self._result
+
+
+
+def what_to_eat(month):
+    time_for_oysters = OystersGood(month)
+    time_for_tomatoes = TomatoesGood(month)
+    if time_for_oysters:
+        print('%s: oysters' % month)
+    elif time_for_tomatoes:
+        print('%s: tomatoes' % month)
+    else:
+        print('%s: asparagus' % month)
+
+
+if __name__ == '__main__':
+    what_to_eat('November')
+    what_to_eat('July')
+    what_to_eat('March')
