06a72c53700b965a0966dc3f57851da06fd50f97
Yann Yu <yann.yu@medlytix.com>
2017-06-28 11:07:14 -0400

    add re-sampling via bootstrap

--- Big_Ideas_Little_Python_Code/part2.2/step_01_bootstraping.py
+++ Big_Ideas_Little_Python_Code/part2.2/step_01_bootstraping.py
@@ -6,4 +6,18 @@
 # Learn more information about our sampling
 # without new samples
 
+# Re-sample with replacement from that same
+# sample you have taken
+# Sometimes sample is expensive to get: 
+#    think drug tests
+#    drug study on effects
+
+# Bootstraping is used when obtaining new
+# samples is expensive
+
+
+
+
+
+
 
