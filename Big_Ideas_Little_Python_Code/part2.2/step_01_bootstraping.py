975e915ef1dd5b9e224ab8327b39b07edfa134e2
Yann Yu <yann.yu@medlytix.com>
2017-06-28 10:26:36 -0400

    add bootstraping - picking yourself up by the bootstrap and lifting yourself over the fence

---
+++ Big_Ideas_Little_Python_Code/part2.2/step_01_bootstraping.py
@@ -0,0 +1,9 @@
+# step_01_bootstraping.py
+
+# Great idea: we can learn charateristics of 
+# a sample of population by re-sampling of
+# the same sample itself
+# Learn more information about our sampling
+# without new samples
+
+
