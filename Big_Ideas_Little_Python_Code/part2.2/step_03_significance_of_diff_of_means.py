27baea363ba13f2740b33cf6d4132ce309d61e53
yannhyu <yannhyu@yahoo.com>
2017-07-11 17:37:19 -0400

    Create step_03_significance_of_diff_of_means.py

---
+++ Big_Ideas_Little_Python_Code/part2.2/step_03_significance_of_diff_of_means.py
@@ -0,0 +1,9 @@
+from statistics import mean, stdev
+
+drug = [7.1, 8.5, 6.4, 7.7, 8.2, 7.6, 8.4, 5.1, 8.1, 7.4, 6.9, 8.4]
+placebo = [8.2, 6.1, 7.1, 7.1, 4.9, 7.4, 8.1, 7.1, 6.2, 7.0, 6.6, 6.3]
+
+print(mean(drug))
+print(mean(placebo))
+obs_diff = mean(drug) - mean(placebo)
+print(obs_diff)
