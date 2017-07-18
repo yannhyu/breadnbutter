066b80c71f5cde1b583b109ebf53e984686403f4
Yann Yu <yann.yu@medlytix.com>
2017-07-18 14:52:49 -0400

    add build trials

--- Big_Ideas_Little_Python_Code/part2.2/step_05_p_values.py
+++ Big_Ideas_Little_Python_Code/part2.2/step_05_p_values.py
@@ -4,6 +4,7 @@
 # due to real facts??
 
 from statistics import mean, stdev
+from random import shuffle
 
 # birth weights from group given the drug
 drug = [7.1, 8.5, 6.4, 7.7, 8.2, 7.6, 8.4, 5.1, 8.1, 7.4, 6.9, 8.4]
@@ -17,3 +18,49 @@ print(mean(drug))
 print(mean(placebo))
 obs_diff = mean(drug) - mean(placebo)
 print(f'difference between the drug mean and placebo mean is {obs_diff}')
+
+# it raises the question: was that difference real
+# or was it due to chance??
+
+# One way to find out is called permutation test:
+# assuming NULL hypothesis, (totally due to chance)
+# we ought to be freely swapping the participants 
+# between the two controlled groups and get the same
+# results: as extreme as we observed here.
+
+# Re-shuffling the data
+# Taking the participants from the two groups and
+# randomly re-label them, or re-assign them to the
+# groups; or put it another way: when we permute it
+# how often does it change the outcome?
+
+# first combine the two groups
+comb = drug + placebo
+
+# number of drug group
+nd = len(drug)
+
+shuffle(comb)
+
+# get the new drug group
+print(comb[:nd])
+
+# get the new placebo group
+print(comb[nd:])
+
+# If we shuffle (permuting or relabeling) the participants,
+# is the new mean diff the same or more extreme than we
+# observed?
+
+def trial():
+    shuffle(comb)
+    drug = comb[:nd]
+    placebo = comb[nd:]
+    new_diff = mean(drug) - mean(placebo)
+    return new_diff >= obs_diff
+
+# our usual minimum repeat trial times
+n = 10000
+
+avg_of_trials = sum(trial() for i in range(n)) / n
+print(f'Our average of trials is {avg_of_trials}')
